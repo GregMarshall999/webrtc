1ad58956f3297b49cf41fd4ec0d6a3696d4837eb
Gregory Marshall <gmarshall@dawan.fr>
2024-06-10 17:00:44 +0200

    agora rtm signaling

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+//TODO: create signaling server backend (build signaling server)
+
+const APP_ID = "ec50216c11144505a73823ced15d3f80";
+
+let token = null;
+let uid = String(Math.floor(Math.random() * 10000)); //replace by user ID of the current connected user
+
+let client;
+let channel;
+
 let localStream;
 let remoteStream;
 let peerConnection;
@@ -11,17 +21,30 @@ const servers = {
 }
 
 let init = async () => {
+    client = await AgoraRTM.createInstance(APP_ID); //TODO: adapt to latest version
+    await client.login({ uid, token });
+
+    channel = client.createChannel('main');
+    await channel.join()
+
+    channel.on('MemberJoined', handleUserJoined)
+
+    client.on('MessageFromPeer', handleMessageFromPeer);
+
     localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
     document.getElementById('user-1').srcObject = localStream;
-
-    createOffer();
 }
 
-let createOffer = async () => {
+let createOffer = async memberId => {
     peerConnection = new RTCPeerConnection(servers);
 
     remoteStream = new MediaStream();
     document.getElementById('user-2').srcObject = remoteStream;
+
+    if(!localStream) {
+        localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
+        document.getElementById('user-1').srcObject = localStream;
+    }
 
     localStream.getTracks().forEach(track => {
         peerConnection.addTrack(track, localStream);
@@ -35,14 +58,25 @@ let createOffer = async () => {
 
     peerConnection.onicecandidate = async event => {
         if(event.candidate) {
-            console.log('New ICE Candidate:', event.candidate);
+            client.sendMessageToPeer({ text: JSON.stringify({ 'type':'candidate', 'candidate':event.candidate }) }, memberId);
         }
     };
 
     let offer = await peerConnection.createOffer();
     await peerConnection.setLocalDescription(offer);
 
-    console.log('Offer:', offer);
+    //console.log('Offer:', offer);
+    client.sendMessageToPeer({ text: JSON.stringify({ 'type':'offer', 'offer':offer }) }, memberId);
+}
+
+let handleUserJoined = async memberId => {
+    console.log('A new user joined the channel:', memberId);
+    createOffer(memberId);
+}
+
+let handleMessageFromPeer = async (message, memberId) => {
+    msg = JSON.parse(message.text);
+    console.log('Message:', msg);
 }
 
 init();
