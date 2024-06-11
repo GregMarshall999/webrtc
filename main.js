3d1cefa9ee19ae01ad04570365a10d9de6d87ef9
Gregory Marshall <gmarshall@dawan.fr>
2024-06-11 11:47:13 +0200

    create rooms

--- main.js
+++ main.js
@@ -8,6 +8,14 @@ let uid = String(Math.floor(Math.random() * 10000)); //replace by user ID of the
 let client;
 let channel;
 
+let queryString = window.location.search;
+let urlParams = new URLSearchParams(queryString);
+let roomId = urlParams.get('room');
+
+if(!roomId) {
+    window.location = 'lobby.html';
+}
+
 let localStream;
 let remoteStream;
 let peerConnection;
@@ -24,7 +32,7 @@ let init = async () => {
     client = await AgoraRTM.createInstance(APP_ID); //TODO: adapt to latest version
     await client.login({ uid, token });
 
-    channel = client.createChannel('main');
+    channel = client.createChannel(roomId);
     await channel.join()
 
     channel.on('MemberJoined', handleUserJoined);
