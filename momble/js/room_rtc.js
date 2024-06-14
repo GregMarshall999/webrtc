1b45a4258406c89d2936d43d9834e5ad86faf3ad
Gregory Marshall <gmarshall@dawan.fr>
2024-06-14 14:35:23 +0200

    camera quality settings

--- momble/js/room_rtc.js
+++ momble/js/room_rtc.js
@@ -31,7 +31,12 @@ let joinRoomInit = async () => {
 }
 
 let joinStream = async () => {
-    localTracks = await AgoraRTC.createMicrophoneAndCameraTracks();
+    localTracks = await AgoraRTC.createMicrophoneAndCameraTracks(
+        {}, 
+        { encoderConfig: {
+                width: { min: 640, ideal: 1920, max: 1920 }, 
+                height: { min: 480, ideal: 1080, max: 1080 }
+        }});
 
     let player = `  <div class="video_container" id="user-container-${uid}">
                         <div class="video-player" id="user-${uid}"></div>
