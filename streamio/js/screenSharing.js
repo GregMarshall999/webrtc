bf6c8f4327aa7fe157df87aee9ca784118125ca1
Gregory Marshall <gmarshall@dawan.fr>
2024-07-03 09:42:15 +0200

    screen sharing

---
+++ streamio/js/screenSharing.js
@@ -0,0 +1,27 @@
+const shareButton = document.getElementById('shareButton');
+
+shareButton.addEventListener('click', () => {
+    navigator.mediaDevices.getDisplayMedia({audio: true, video: true})
+        .then(shareScreen, onCatch);
+});
+
+function shareScreen(stream) {
+    shareButton.disabled = true;
+
+    const video = document.querySelector('video');
+    video.srcObject = stream;
+
+    stream.getVideoTracks()[0].addEventListener('ended', () => {
+        shareButton.disabled = false;
+    });
+}
+
+// Function to handle errors during media stream acquisition
+function onCatch(error) {
+    const errorElement = document.querySelector('#errorMsg');
+    errorElement.innerHTML += `<p>Something went wrong: ${error.name}</p>`;
+}
+
+if ((navigator.mediaDevices && 'getDisplayMedia' in navigator.mediaDevices)) {
+    shareButton.disabled = false;
+}
